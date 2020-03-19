178fc8713f174980157894fdf6e1d318fc1c219d
SebG-prog <sebastien-gruet@orange.fr>
2020-03-19 11:30:22 +0100

    repeat.js fixed

--- exercises/repeat.js
+++ exercises/repeat.js
@@ -8,6 +8,14 @@
  */
 
 // Your code:
+const repeat = (string, n) => {
+	let result = ''
+	for (let i = 0; i < n; i++) {
+		result += string
+	}
+	return result
+}
+
 
 //* Begin of tests
 const assert = require('assert');
