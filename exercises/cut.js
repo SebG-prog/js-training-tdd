898e96ca895da3f552c89857ec270826474026fa
SebG-prog <sebastien-gruet@orange.fr>
2020-03-19 12:09:20 +0100

    cut.js WIP

--- exercises/cut.js
+++ exercises/cut.js
@@ -14,6 +14,9 @@
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.strictEqual(cutFirst.length, 1);
+assert.deepStrictEqual(cutFirst("ceci est un test"), "ci est un test");
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
 // End of tests */
