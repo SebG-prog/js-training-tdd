5cb4034fcc7181da26149aeb5d293bbfd8eaca03
SebG-prog <sebastien-gruet@orange.fr>
2020-03-19 12:03:02 +0100

    whisper.js fixed

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -8,9 +8,15 @@
  */
 
 // Your code:
+const whisper = string => "*" + string.toLowerCase() + "*"
+
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof whisper, 'function');
+assert.strictEqual(whisper.length, 1);
+assert.deepStrictEqual(whisper("CECI EST UN TEST"), "*ceci est un test*");
+assert.deepStrictEqual(whisper("Et un Second Test"), "*et un second test*");
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
 // End of tests */
